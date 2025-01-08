5028cee262486cffc0b015664c15903b18b009c7
Prince527 <Prince527gaming@gmail.com>
2025-01-07 21:10:10 -0400

    Update packages and add flatpak builds

--- forge.config.js
+++ forge.config.js
@@ -34,6 +34,14 @@ module.exports = {
       config: {},
     },
     {
+      name: "@electron-forge/maker-flatpak",
+      config: {
+        options: {
+          categories: ["Audio"]
+        }
+      }
+    },
+    {
       name: "@forkprince/electron-forge-maker-appimage",
       platforms: ["linux"],
       config: {
