3c29afbf692ce87b1eedb4a3b4008d1163f92ff8
Prince527 <Prince527gaming@gmail.com>
2025-05-31 11:47:08 -0300

    Remove AppX

--- forge.config.js
+++ forge.config.js
@@ -22,15 +22,11 @@ module.exports = {
         name: "ReLunaticPlayer"
       },
     },
-    // {
-    //   name: "@electron-forge/maker-wix",
-    //   config: {
-    //     icon: `${icon}.ico`
-    //   },
-    // },
     {
-      name: "@electron-forge/maker-appx",
-      config: {},
+      name: "@electron-forge/maker-wix",
+      config: {
+        icon: `${icon}.ico`
+      },
     },
     {
       name: "@electron-forge/maker-zip",
